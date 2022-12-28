723f4dfb23bad11ba564913ff2b02750f99fa2df
nikamgebroo <nmghe2020@agruni.edu.ge>
2022-12-28 18:02:34 +0400

    some front-end changes in product add page

--- main.php
+++ main.php
@@ -21,30 +21,36 @@ $products =$statement->fetchALL(PDO::FETCH_ASSOC);
     <div class ="mainProductAdd">
         <button><a href="productAdd.php">ADD</a></button>
     </div>
-    <div id="delete-product-btn">
-        <form method="post" action="delete.php">
-        <button id="delete_button">Mass delete</button>
-        </form>
-    </div>
-
 </div>
     <div class="checkboxes">
+
         <?php foreach ($products as $i=> $product) { ?>
-           <form action="" method="post">
+           <form name="form1" action="" method="post">
             <div class="card">
-                <input type="checkbox" class="delete-checkbox" />
                 <div class="card-body">
+                    <input type="checkbox" name="num[]" class="other" value="<?php echo $product["id"]?>" />
                     <h5><?php echo $product['SKU']?></h5>
                     <h5><?php echo $product['name'] ?></h5>
                     <h5><?php echo $product['price'] ?></h5>
                     <h5><?php echo $product['attribute_value_id'] ?></h5>
                 </div>
             </div>
-           </form>
+
         <?php } ?>
-
+        <div id="delete-product-btn">
+            <input  type="submit" name="submit1" value="Mass Delete">
+        </div>
     </div>
+    </form>
 </div>
-
+<?php if(isset($_POST["submit1"])){
+    $box=$_POST['num'];
+    foreach ($box as $key => $val) {
+        $statement = $pdo->prepare('DELETE FROM products_table WHERE id= :id ' );
+        $statement->bindValue(':id', $val);
+        $statement->execute();
+        header('Location: main.php'); exit();
+    }
+} ?>
 </body>
 </html>
