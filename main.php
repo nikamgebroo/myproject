164c9641923bb6818c36060f92c5bf1f698fbcf9
nikamgebroo <nmghe2020@agruni.edu.ge>
2022-12-28 16:12:55 +0400

    some front-end changes in product add page

--- main.php
+++ main.php
@@ -22,24 +22,29 @@ $products =$statement->fetchALL(PDO::FETCH_ASSOC);
         <button><a href="productAdd.php">ADD</a></button>
     </div>
     <div id="delete-product-btn">
+        <form method="post" action="delete.php">
         <button id="delete_button">Mass delete</button>
+        </form>
+    </div>
+
+</div>
+    <div class="checkboxes">
+        <?php foreach ($products as $i=> $product) { ?>
+           <form action="" method="post">
+            <div class="card">
+                <input type="checkbox" class="delete-checkbox" />
+                <div class="card-body">
+                    <h5><?php echo $product['SKU']?></h5>
+                    <h5><?php echo $product['name'] ?></h5>
+                    <h5><?php echo $product['price'] ?></h5>
+                    <h5><?php echo $product['attribute_value_id'] ?></h5>
+                </div>
+            </div>
+           </form>
+        <?php } ?>
+
     </div>
 </div>
 
-</div>
-<div class="checkboxes">
-    <?php foreach ($products as $i=> $product) { ?>
-    <div class="card">
-        <input type="checkbox" class="delete-checkbox" />
-        <div class="card-body">
-         <h5><?php echo $product['SKU']?></h5>
-            <h5><?php echo $product['name'] ?></h5>
-            <h5><?php echo $product['price'] ?></h5>
-            <h5><?php echo $product['attribute_value_id'] ?></h5>
-        </div>
-    </div>
-<?php } ?>
-
-</div>
 </body>
 </html>
