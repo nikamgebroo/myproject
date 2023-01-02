63ab53b7e3854e8ef956e9e3d5b2bdf4daf64600
nikamgebroo <nmghe2020@agruni.edu.ge>
2023-01-03 02:26:06 +0400

    product add to database (based)

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products', 'root', '');
 $pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$statement = $pdo->prepare('SELECT * FROM products_table ORDER BY SKU DESC' );
+$statement = $pdo->prepare('SELECT * FROM products_table INNER JOIN attribute_values ON products_table.attribute_value_id=attribute_values.id  ORDER BY SKU DESC' );
 $statement->execute();
 $products =$statement->fetchALL(PDO::FETCH_ASSOC);
 $value='';
@@ -28,16 +28,11 @@ $value='';
         <form name="form1" method="post">
             <div class="card">
                 <div class="card-body">
-                    <input type="checkbox" name="num[]" class="other" value="<?php echo $product["id"]?>" />
+                    <input type="checkbox" name="num[]" class="other" value="<?php echo $product["attribute_value_id"]?>" />
                     <h5><?php echo $product['SKU']?></h5>
                     <h5><?php echo $product['name'] ?></h5>
-                    <h5><?php echo $product['price'] ?></h5>
-                    <h5><?php
-                        $st = $pdo->prepare('SELECT max(value) FROM attribute_values  ORDER BY ID DESC ');
-                        $st->execute();
-                        $value = $st->fetchColumn();
-                        echo $value;
-                         ?></h5>
+                    <h5><?php echo $product['price'] . "$" ?></h5>
+                    <h5><?php echo $product['value']; ?></h5>
                 </div>
             </div>
             <?php }
@@ -52,7 +47,7 @@ $value='';
 if(isset($_POST["submit1"])){
     $deleteProduct = $_POST['num'];
     foreach ($deleteProduct as $id) {
-        $sql = "DELETE FROM attribute_values WHERE id = :id";
+        $sql = "DELETE FROM attribute_values WHERE id = ?";
         $stmt = $pdo->prepare($sql);
         $stmt->execute([$id]);
         header('location: main.php');
