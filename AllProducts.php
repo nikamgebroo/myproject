8c87c49ff8eeb02beaeace02808fbc5a7728f867
nikamgebroo <nmghe2020@agruni.edu.ge>
2023-01-07 23:04:44 +0400

    Fixing all

--- AllProducts.php
+++ AllProducts.php
@@ -1,6 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products', 'root', '');
-$pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 
 
 abstract class allProducts
@@ -10,33 +9,4 @@ abstract class allProducts
     public string $name;
     public float $price;
     public int $attribute_value_id;
-}
-if($_SERVER['REQUEST_METHOD'] ==='POST'){
-    var_dump($_POST);
-    $sku =$_POST['SKU'];
-    $name =$_POST['name'];
-    $price =$_POST['price'];
-    $attribute_value_id =$_POST['attribute_value_id'];
-
-
-
-    if(!$sku ){
-        $errors[]='product title is required';
-    }
-    if(!$name ){
-        $errors[]='product price is required';
-    }
-    if(!$price ){
-        $errors[]='product price is required';
-    }
-    if(empty(($errors))){
-        $statement = $pdo->prepare("INSERT INTO products(SKU,name, price , price,attribute_value_id)
-                    VALUES(:SKU, :name, :price, :attribute_value_id)");
-        $statement->bindValue(':SKU', $sku);
-        $statement->bindValue(':name', $name);
-        $statement->bindValue(':price', $price);
-        $statement->bindValue(':attribute_value_id', $attribute_value_id);
-        $statement->execute();
-        header('location: main.php');
-    }
 }
--- productAdd.php
+++ productAdd.php
@@ -1,5 +1,67 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+$errors = [];
+$SKU = '';
+$name = '';
+$price = '';
+$attribute_value_id = '';
+$value = '';
+$type_id = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $SKU = $_POST['SKU'];
+    $name = $_POST['name'];
+    $price = $_POST['price'];
+
+    if (isset($_POST['submitAdd'])) {
+        if (!empty($_POST['productType'])) {
+            $selected = $_POST['productType'];
+            $st = $pdo->prepare("SELECT id FROM types_table WHERE type_name=:type_name LIMIT 1");
+            $st->bindParam(":type_name", $selected);
+            $st->execute();
+            $type_id = $st->fetchColumn();
+            $value = $_POST['value' . $selected];
+        } else {
+            echo 'Please select the value.';
+        }
+    }
+    if (!$price) {
+        $errors[] = 'product price is required';
+    }
+    if (true) {
+        $statement = $pdo->prepare("INSERT INTO attribute_values(value,type_id)
+                    VALUES(:value, :type_id)");
+        $statement->bindValue(':value', $value);
+        $statement->bindValue(':type_id', $type_id);
+        $statement->execute();
+
+
+    }
+    $lastInsertId = $pdo->lastInsertId();
+    if (true) {
+        $st = $pdo->prepare("SELECT :id FROM attribute_values LIMIT 1");
+        $st->bindParam(":id", $lastInsertId);
+        $st->execute();
+        $attribute_value_id = $st->fetchColumn();
+    }
+
+    if (true) {
+        $statement = $pdo->prepare("INSERT INTO products_table(SKU,name,price, attribute_value_id)
+                    VALUES(:SKU, :name, :price, :attribute_value_id)");
+        $statement->bindValue(':SKU', $SKU);
+        $statement->bindValue(':name', $name);
+        $statement->bindValue(':price', $price);
+        $statement->bindValue(':attribute_value_id', $attribute_value_id);
+        $statement->execute();
+        header('location: main.php');
+    }
+}
+?>
 <?php require_once "AllProducts.php" ?>
 <?php require_once"productAddHeader.php"?>
+
 <?php  ?>
 <!--Product add form -->
 <form id="product_form" method="post" enctype="multipart/form-data">
@@ -69,3 +131,4 @@
     </div>
 </form>
 <?php require_once "Footer.php"?>
+
