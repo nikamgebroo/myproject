4d3ffb04a1095e835423351e350b73fedc281aa6
nikamgebroo <nmghe2020@agruni.edu.ge>
2023-01-07 20:46:09 +0400

    Changing something

--- Book.php
+++ Book.php
@@ -2,5 +2,5 @@
 include ("AllProducts.php");
 class Book  extends AllProducts
 {
-    public float $weight;
+    public ?int $value;
 }
--- main.php
+++ main.php
@@ -1,13 +1,6 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products', 'root', '');
-$pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$statement = $pdo->prepare('SELECT * FROM products_table INNER JOIN attribute_values ON products_table.attribute_value_id=attribute_values.id INNER JOIN types_table ON attribute_values.type_id=types_table.id ORDER BY SKU DESC');
-$statement->execute();
-$products =$statement->fetchALL(PDO::FETCH_ASSOC);
-$value='';
+require_once "ProductController.php";
 ?>
-
-
 
 <?php require_once "Header.php" ?>
 
@@ -29,15 +22,7 @@ $value='';
     </form>
 </div>
 <?php
-if(isset($_POST["submit1"])){
-    $deleteProduct = $_POST['num'];
-    foreach ($deleteProduct as $id) {
-        $sql = "DELETE FROM attribute_values WHERE id = ?";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$id]);
-        header('location: main.php');
-    }
-}
+
 ?>
 <?php require_once "Footer.php"?>
 
